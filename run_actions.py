7f12f3475d3628c57f3dc5a98389f8bd46076fd6
Vojtěch Perník <develop@pervoj.cz>
2022-06-26 18:56:30 +0200

    Add messages to running actions

--- run_actions.py
+++ run_actions.py
@@ -1,14 +1,32 @@
+from gettext import gettext as _
+import colors
 from actions import actions
 from functions import clear
 
 runners = []
 
-for action in actions:
+for index, action in enumerate(actions):
 	clear()
+	print(
+		colors.OKBLUE +
+		_("[question {} of {}]").format(index + 1, len(actions)) +
+		colors.ENDC
+	)
+	print()
+	print(colors.WARNING + _( # try to keep the text width under 80
+		"If you don't know what the option means, just press Enter to select the default\n"+
+		"answer. The default answers are chosen to suit as many people as possible."
+	) + colors.ENDC)
+	print()
 	runner = action()
 	if (runner != None): runners.append(runner)
 
 clear()
+
+print(colors.WARNING + _( # try to keep the text width under 80
+	"Now wait a while until everything is ready."
+) + colors.ENDC)
+print()
 
 for runner in runners:
 	runner()
