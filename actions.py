62ebcbe13e83d797e65de7197e49a4d08dc0a56a
Vojtěch Perník <develop@pervoj.cz>
2022-06-26 19:30:04 +0200

    Make install actions working

--- actions.py
+++ actions.py
@@ -1,7 +1,9 @@
 from action_parts.repositories import actions as repositories
 from action_parts.dnf import actions as dnf
+from action_parts.installs import actions as installs
 
 actions = []
 
 actions += repositories
 actions += dnf
+actions += installs
