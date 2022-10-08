df116ebb394ed1865754267802e1336c5a967bde
Vojtěch Perník <develop@pervoj.cz>
2022-10-08 09:50:14 +0000

    Add sudo password asterisks enabling action

--- actions.py
+++ actions.py
@@ -1,5 +1,6 @@
 from action_parts.repositories import actions as repositories
 from action_parts.drivers import actions as drivers
+from action_parts.sudo import actions as sudo
 from action_parts.dnf import actions as dnf
 from action_parts.installs import actions as installs
 
@@ -7,5 +8,6 @@ actions = []
 
 actions += repositories
 actions += drivers
+actions += sudo
 actions += dnf
 actions += installs
